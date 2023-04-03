2a7454c3016779cb2e8180d0a50d6aebb800fbc9
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-04-03 11:31:56 +0300

    practise

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div className="container-xl text-white">
+        <h3 className="">REACT APP</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
